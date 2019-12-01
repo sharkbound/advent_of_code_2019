1fbe937c6c6d41b1af4e4bbe17a84f6be0353479
sharkbound <laptopblaster@gmail.com>
2019-12-01 00:38:21 -0600

    changed / to // for day 1

--- day1/part1.py
+++ day1/part1.py
@@ -2,7 +2,7 @@ from read import read_lines
 
 
 def get_fuel(mass):
-    return int(mass / 3) - 2
+    return mass // 3 - 2
 
 
 def solve_part_1(masses):
--- day1/part2.py
+++ day1/part2.py
@@ -2,7 +2,7 @@ from read import read_lines
 
 
 def get_fuel(mass):
-    if (fuel := int(mass / 3) - 2) <= 0:
+    if (fuel := mass // 3 - 2) <= 0:
         return 0
     return fuel + get_fuel(fuel)
 
