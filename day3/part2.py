d4a9f1241f893492d4db5d1ba44ca4861aeb8462
sharkbound <laptopblaster@gmail.com>
2019-12-03 12:21:46 -0600

    added wire graph, and graphing code using matplotlib

--- day3/part2.py
+++ day3/part2.py
@@ -3,6 +3,8 @@ from typing import Tuple, NamedTuple, Iterator, Dict
 
 from read import read_lines
 
+GRAPH = True
+
 
 def dist(x: int, y: int) -> int:
     return abs(x) + abs(y)
@@ -10,6 +12,9 @@ def dist(x: int, y: int) -> int:
 
 class Wire(NamedTuple):
     dists: Dict[Tuple[int, int], int]
+    # used for graphing
+    #                  dir  steps x_off, y_off total x_off  total y_off
+    turns: Tuple[Tuple[str, int, int, int, int, int]]
 
 
 def turns(line: str) -> Iterator[Tuple[str, int]]:
@@ -17,10 +22,31 @@ def turns(line: str) -> Iterator[Tuple[str, int]]:
         yield direction, int(steps)
 
 
+def graph(*wires):
+    if not GRAPH:
+        return
+
+    import matplotlib.pyplot as plt
+
+    colors = iter('rg')
+
+    for wire in wires:
+        color = next(colors)
+        last = 0, 0
+
+        for turn in wire.turns:
+            _, _, _, _, xoff, yoff = turn
+            plt.plot([last[0], last[0] + xoff], [last[1], last[1] + yoff], color)
+            last = last[0] + xoff, last[1] + yoff
+
+    plt.show()
+
+
 def parse_wire(line: str) -> Wire:
     x = y = 0
     distances = {}
     length = 0
+    wire_turns = []
     for dir, steps in turns(line):
         y_off = x_off = 0
         if dir == 'U':
@@ -31,17 +57,19 @@ def parse_wire(line: str) -> Wire:
             x_off = -1
         elif dir == 'R':
             x_off = 1
+        wire_turns.append((dir, steps, x_off, y_off, x_off * steps, y_off * steps))
         for _ in range(steps):
             x += x_off
             y += y_off
             length += 1
             if (x, y) not in distances:
                 distances[x, y] = length
-    return Wire(distances)
+    return Wire(distances, tuple(wire_turns))
 
 
 def solve_part_2(data):
     w1, w2 = map(parse_wire, data)
+    graph(w1, w2)
     return min(w1.dists[xy] + w2.dists[xy] for xy in w1.dists.keys() & w2.dists)
 
 
