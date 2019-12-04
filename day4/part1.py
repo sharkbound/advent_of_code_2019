21da74b4d4fe17ba60c0bafa83d13a3445b10978
sharkbound <laptopblaster@gmail.com>
2019-12-04 00:42:44 -0600

    use sorted for checking that a number is sorted properly

--- day4/part1.py
+++ day4/part1.py
@@ -3,18 +3,9 @@ import re
 from read import read
 
 
-def is_increasing_or_eq(i):
-    last = int(i[0])
-    for digit in map(int, i[1:]):
-        if digit < last:
-            return False
-        last = digit
-    return True
-
-
 def solve_part_1(data):
     start, end = map(int, data.split('-'))
-    return sum(1 for i in map(str, range(start, end + 1)) if re.search(r'(\d)\1', i) and is_increasing_or_eq(i))
+    return sum(1 for i in map(str, range(start, end + 1)) if re.search(r'(\d)\1', i) and ''.join(sorted(i)) == i)
 
 
 def main():
--- day4/part2.py
+++ day4/part2.py
@@ -3,22 +3,13 @@ import re
 from read import read
 
 
-def is_increasing_or_eq(i):
-    last = int(i[0])
-    for digit in map(int, i[1:]):
-        if digit < last:
-            return False
-        last = digit
-    return True
-
-
 def has_double(i):
     return any(m for m in re.finditer(r'(\d)(\1+)', i) if len(''.join(m.groups())) == 2)
 
 
 def solve_part_2(data):
     start, end = map(int, data.split('-'))
-    return sum(1 for i in map(str, range(start, end + 1)) if (has_double(i) and is_increasing_or_eq(i)))
+    return sum(1 for i in map(str, range(start, end + 1)) if (has_double(i) and ''.join(sorted(i)) == i))
 
 
 def main():
