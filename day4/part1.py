f539527dff296b069636862d8cbb6db9cbdc55ab
sharkbound <laptopblaster@gmail.com>
2019-12-03 23:40:16 -0600

    day 4 done

--- day4/part1.py
+++ day4/part1.py
@@ -1,14 +1,36 @@
+import re
+
 from read import read, read_lines
 
 
-def solve_part_(data):
-    pass
+def solve_part_1(data):
+    start, end = map(int, data.split('-'))
+    password_range = range(start, end + 1)
+    count = 0
+
+    for i in map(str, password_range):
+        if not re.search(r'(\d)\1', i):
+            continue
+
+        last = int(i[0])
+        bad = False
+        for digit in map(int, i[1:]):
+            if digit < last:
+                bad = True
+                break
+            last = digit
+
+        if bad:
+            continue
+
+        count += 1
+    return count
 
 
 def main():
     data = read()
-    print(solve_part_(data))
-    
-    
+    print(solve_part_1(data))
+
+
 if __name__ == '__main__':
     main()
--- day4/part2.py
+++ day4/part2.py
@@ -1,14 +1,41 @@
+import re
+
 from read import read, read_lines
 
 
-def solve_part_(data):
-    pass
+def solve_part_2(data):
+    start, end = map(int, data.split('-'))
+    password_range = range(start, end + 1)
+    count = 0
+
+    for i in map(str, password_range):
+        double = False
+        for m in re.finditer(r'(\d)(\1+)', i):
+            s = ''.join(m.groups())
+            double = double or len(s) == 2
+
+        if not double:
+            continue
+
+        last = int(i[0])
+        bad = False
+        for digit in map(int, i[1:]):
+            if digit < last:
+                bad = True
+                break
+            last = digit
+
+        if bad:
+            continue
+
+        count += 1
+    return count
 
 
 def main():
     data = read()
-    print(solve_part_(data))
-    
-    
+    print(solve_part_2(data))
+
+
 if __name__ == '__main__':
     main()
