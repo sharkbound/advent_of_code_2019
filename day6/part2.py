939a3d1877725c28863997bdc1a2b4343bb9652d
sharkbound <laptopblaster@gmail.com>
2019-12-12 17:44:42 -0600

    added comments to day 6 code

--- day6/part2.py
+++ day6/part2.py
@@ -1,62 +1,65 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 from read import read_lines
 
 
-# def iter_orbit_branches(orbits):
-#     total = 0
-#     for base_orbit in orbits:
-#         queue = deque([base_orbit])
-#         while queue:
-#             value = queue.popleft()
-#             subs = orbits.get(value, [])
-#             total += len(subs)
-#             queue.extend(subs)
-#
-#     return total
-
-def walk_orbits(orbits):
-    print(orbits)
-
-
 def path_to_root(orbits, start):
+    """
+    walks the path from the `start` node to the root node
+    keep doing this until we cannot find any more parents
+    """
     path = [start]
     cur = start
+    # python 3.8 "walrus operator" is used here to make the loop more condensed
     while cur := find_parent(orbits, cur):
         path.append(cur)
     path.reverse()
     return path
 
 
-YOU, SAN = 'YOU', 'SAN'
+def path_len(path: list, start: str):
+    """
+    returns the length of the given path, starting from node `start`
+    skip the last node using -1 in the slice because that in the node of interest (aka SAN or YOU)
+    """
+    return len(path[path.index(start):-1])
 
 
-def path_len(path: list, start: str, sub=0):
-    l = path[path.index(start):-1]
-    # print(f'{l} {len(l)-sub} {sub}')
-    return len(l) - sub
+def find_parent(orbits, start) -> str:
+    """
+    tries to find the parent of `start` node
+    we go about doing this by going through the orbit dict and looking for it in the it's sub-orbits
+    when we find one return that parent node
+    if no parent node is found, return None
+    """
+    return next((key for key, value in orbits.items() if start in value), None)
 
 
 def solve(data):
+    # contains the sub-orbits of nodes
     orbits = defaultdict(list)
+    # get all orbits in input
     for o1, o2 in (s.split(')') for s in data):
+        # appends the orbit to its parent in the defaultdict
         orbits[o1].append(o2)
 
-    py = path_to_root(orbits, YOU)
-    ps = path_to_root(orbits, SAN)
+    # get the paths to the locations of interest
+    path_to_you = path_to_root(orbits, 'YOU')
+    path_to_san = path_to_root(orbits, 'SAN')
 
+    # store path lengths to find lowest length after loop
     counts = []
-    for common in set(py) & set(ps):
-        total = path_len(py, common, 1) + path_len(ps, common, 1)
+    # loop over all the the common nodes in the paths
+    # convert the paths the SET's to allow use of SET INTERSECTION to find all common path nodes
+    for common in set(path_to_you) & set(path_to_san):
+        # get the total path length,
+        total = path_len(path_to_you, common) + path_len(path_to_san, common) - 2
+        # add the path total path length to the path length list to min later
         counts.append(total)
+        # python 3.8 f-string debug print, optional
         print(f'{common=} {total=}')
-
+    # print the best(MIN) path length found
     return f'==================\nBEST = {min(counts)}\n=================='
-    # print(f'{py}\n{ps}')
-
-
-def find_parent(orbits, to_find) -> str:
-    return next((key for key, value in orbits.items() if to_find in value), None)
 
 
 def main():
