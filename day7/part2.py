6b75c9174b2592b96fd23f840f12c24ef570d78e
sharkbound <laptopblaster@gmail.com>
2019-12-20 14:24:18 -0600

    small best fix

--- day7/part2.py
+++ day7/part2.py
@@ -1,39 +1,37 @@
+from collections import deque
 from copy import copy
 from itertools import permutations, chain
 
 from read import read
 from shared.intcode import *
 
-enable_logging()
-disable_logging()
+
+# enable_logging()
 
 
 def solve(data):
+    last_output = best_output = 0
+    inputs = deque([])
+
+    set_input_provider(inputs.popleft)
+
+    @set_output_logger
+    def output(v):
+        nonlocal last_output
+        inputs.appendleft(v)
+        print(f'last: {str(last_output)[:30]}')
+
     for modes in map(list, permutations(range(5))):
-        @set_output_logger
-        def output(v):
-            nonlocal last_output
-            last_output = v
 
-        last_output = best_output = 0
         for mode in modes:
-            for extra_modes in permutations(range(5, 10)):
-                for phase in [mode, *extra_modes]:
-                    inputs = [last_output, phase]
-
-                    @set_output_logger
-                    def output(v):
-                        nonlocal last_output
-                        inputs.append(v)
-                        last_output = v
-                        print(f'last(trimmed): {str(last_output)[:30]}')
-
-                    set_input_provider(inputs.pop)
+            for part2_modes in permutations(range(5, 10)):
+                for phase in [mode, *part2_modes]:
+                    inputs.extendleft([phase, last_output])
 
                     execute(data.copy())
                     best_output = max(best_output, last_output)
 
-            print(best_output)
+        print(best_output)
 
 
 def main():
