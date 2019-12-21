2919c5fc13742420accee25b0fae64463892c7b1
sharkbound <laptopblaster@gmail.com>
2019-12-20 23:55:00 -0600

    small change to d7p1

--- day7/part1.py
+++ day7/part1.py
@@ -6,15 +6,14 @@ from shared.intcode import IntCode
 
 def solve(data):
     all_output = set()
-    for perms in permutations(range(5)):
-        perms = list(perms)
+    for phases in permutations(range(5)):
         last_output = 0
 
         def output(v):
             nonlocal last_output
             last_output = v
 
-        for phase in perms:
+        for phase in phases:
             IntCode(data, [phase, last_output], foutput=output).run()
 
         all_output.add(last_output)
