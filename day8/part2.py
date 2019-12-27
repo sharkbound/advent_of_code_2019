ac0c1bf042b1533e381a8a522839bc0dc4703a0b
sharkbound <laptopblaster@gmail.com>
2019-12-26 19:07:01 -0600

    removed dtype

--- day8/part2.py
+++ day8/part2.py
@@ -5,50 +5,6 @@ import numpy as np
 
 from read import read
 
-
-@dataclass(frozen=True)
-class Layer:
-    width: int
-    height: int
-    raw: np.ndarray
-    data: np.ndarray
-
-    @property
-    def size(self):
-        return self.width * self.height
-
-    def __getitem__(self, row_index: int) -> np.ndarray:
-        return self.data[row_index]
-
-
-@dataclass()
-class Image:
-    width: int
-    height: int
-    raw_data: np.ndarray
-    layers: List[Layer] = field(default=list)
-
-    def __post_init__(self):
-        self.layers = [
-            Layer(self.width, self.height, arr.flatten(), arr)
-            for arr in map(np.array, [
-                self.raw_data[i:i + self.chunk_size].reshape((self.height, self.width))
-                for i in range(0, len(self.raw_data), self.chunk_size)
-            ])
-        ]
-
-    @property
-    def chunk_size(self):
-        return self.width * self.height
-
-    def __getitem__(self, layer_index: int) -> Layer:
-        return self.layers[layer_index]
-
-    def __iter__(self) -> Layer:
-        for row_index in range(len(self.raw_data) // self.chunk_size):
-            yield self[row_index]
-
-
 SIZE = HEIGHT, WIDTH = 6, 25
 
 
@@ -64,7 +20,7 @@ def solve(data):
         for arr in np.array_split(data, len(data) // (WIDTH * HEIGHT))
     ])
 
-    final = np.zeros(SIZE, dtype=np.int)
+    final = np.zeros(SIZE)
     for i in np.ndindex(SIZE):
         final[i] = first_non_transparent(layer[i] for layer in layers)
 
